b3a1ef78c3cba14206835f9747d1f36dfb371ddd
mrtyildiz <muratasnb@hotmail.com>
2022-09-06 10:22:57 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,21 +1,43 @@
 import json
+from socket import AF_ECONET
 
 file_name = "/var/log/messages"
 file = open(file_name, "r")
 data = []
-order = ["moon", "Days", "clock", "host", "hsmd", "message", "HSMCommand","HSMCommand2"]
+dataJson = []
+order = ["Date", "Clock", "Host", "HSMD", "Message", "HSMCommand"]
 
 def LogginParse():
-    dataJson = []
     for line in file.readlines():
         details = line.split(" ")
-        details = [x.strip() for x in details]
-        structure = {key:value for key, value in zip(order, details)}
-        data.append(structure)
+        if details[4] != 'hsmd:':
+            pass
+        else:
+            Array = []
+            deta = [x.strip() for x in details]
+            Datet = str(deta[1])+" "+str(deta[0])
+            Array.append(Datet)
+            Clock = str(deta[2])
+            Array.append(Clock)
+            Host = str(deta[3])
+            Array.append(Host)
+            HSMD = str(deta[4])
+            Array.append(HSMD)
+            Message = str(deta[5])
+            Array.append(Message)
+            if 5 <= len(deta):
+                x = len(deta) - 5
+                HSMCommand = ""
+                for i in range(x):
+                    HSMCommand = str(HSMCommand)+deta[5+i]+" "
+            else:
+                pass
+            Array.append(HSMCommand)
+            structure = {key:value for key, value in zip(order, Array)}
+            data.append(structure)
     for entry in data:
-        HSM_Json = json.dumps(entry, indent = 4)
+        HSM_Json = json.dumps(entry, indent = 5)
         y = json.loads(HSM_Json)
         dataJson.append(y)
-    y = json.loads(HSM_Json)
-    return dataJson
-LogginParse()
+    dataJson.reverse()
+    return(dataJson)
